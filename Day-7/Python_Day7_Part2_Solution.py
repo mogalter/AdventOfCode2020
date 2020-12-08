fe9f16e657246ec1229d0114613e5c08f3f74e0b
unknown <kevin.lin@nyu.edu>
2020-12-08 02:04:04 +0000

    Small addition of comments

--- Day-7/Python_Day7_Part2_Solution.py
+++ Day-7/Python_Day7_Part2_Solution.py
@@ -19,6 +19,7 @@ def load_rules():
 def count_bags(cur_color, rules, seen):
 	color_only, color_count = cur_color
 	if not rules[color_only]:
+		# if there's no children, simply return the bag count
 		return color_count
 	else:
 		seen[color_only] = 0
@@ -27,7 +28,7 @@ def count_bags(cur_color, rules, seen):
 			print("Computed" ,color[0], "in", color_only, "-", computed)
 			seen[color_only] += computed
 			print(color_only, "currently has", seen[color_only], "bags in it.")
-		# factor in the original bag
+		# factor in the original bag(s)
 		seen[color_only] += color_count
 		return seen[color_only]
 
