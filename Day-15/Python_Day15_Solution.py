ee19656413a6f18565b9427ffedfb65a6efda391
unknown <kevin.lin@nyu.edu>
2020-12-15 17:57:50 +0000

    Day 15 solutions

---
+++ Day-15/Python_Day15_Solution.py
@@ -0,0 +1,34 @@
+def load_init_nums(filename):
+	init_order =  {}
+	with open(filename, "r") as init_nums:
+		nums = init_nums.readline().strip().split(",")
+		for turn, num in enumerate(nums):
+			turn = int(turn) + 1
+			num = int(num)
+			init_order[num] = turn
+		# return next number
+		return num, init_order
+
+def chant_numbers(prev_num: int, chanted: dict, limit: int):
+	turn = chanted[prev_num] + 1
+	next_to_chant = 0
+	# 4th turn, chanted 0
+	while turn < limit:
+		if next_to_chant in chanted:
+			# case in which we've seen this
+			last_announced = chanted[next_to_chant]
+			chanted[next_to_chant] = turn
+			next_to_chant = turn - last_announced
+
+		else:
+			chanted[next_to_chant] = turn
+			next_to_chant= 0
+		turn += 1
+	print(next_to_chant)
+
+if __name__ == "__main__":
+	prev_num, init_nums = load_init_nums("sample.txt")
+	limit = 30000000
+	print(prev_num, init_nums)
+	chant_numbers(prev_num, init_nums, limit)
+
